2c62edee8c0766124b55269f9be6b505e034a335
Robin Fernandes <rfernandes@atlassian.com>
2022-11-03 09:45:57 +1100

    Add support for bezier curves (easing in & out)

--- src/parseq-lang-interpreter.js
+++ src/parseq-lang-interpreter.js
@@ -2,6 +2,7 @@ import nearley from 'nearley';
 import getGrammar from './parseq-lang.js';
 import { linear, polynomial, step } from 'everpolate';
 import Spline from 'cubic-spline';
+import BezierEasing from "bezier-easing";
 
 
 export function defaultInterpolation(definedFrames, definedValues, frame) {
@@ -28,6 +29,7 @@ function step_interpolation(definedFrames, definedValues, frame) {
 export class InterpreterContext {
   constructor (context) {
     this.fieldName = context.fieldName;
+    this.thisKf = context.thisKf;
     this.definedFrames = context.definedFrames;
     this.definedValues = context.definedValues;
     this.FPS = context.FPS;
@@ -44,7 +46,7 @@ export function parse(input) {
 // Evaluation of parseq lang
 // Returns: a function that takes a frame number and returns a float value
 export function interpret(ast, context) {
-  //console.log("Interpreting: ", ast, context);
+  console.log("Interpreting: ", ast, context);
 
   if (typeof ast === 'number') {
     // Node was interpreted down to a constant.
@@ -52,11 +54,11 @@ export function interpret(ast, context) {
   }
 
   // TODO rewrite in Typescript to avoid this manual type checking nonsense.
-  if (context === "undefined"
-    || context.fieldName === "undefined"
-    || context.thisKf === "undefined"
-    || context.definedFrames === "undefined"
-    || context.definedValues === "undefined") {
+  if (typeof context === "undefined"
+    || typeof context.fieldName === "undefined"
+    || typeof context.thisKf === "undefined"
+    || typeof context.definedFrames === "undefined"
+    || typeof context.definedValues === "undefined") {
     throw new Error(`Invalid context when interpreting ${ast}: ${context}`);
   }
 
@@ -76,6 +78,14 @@ export function interpret(ast, context) {
           return f => cubic_spline_interpolation(context.definedFrames, context.definedValues, f);
         case 'f':
           return f => f;
+        case 'prev_keyframe':
+          return f => getPreviousKeyframe(context, f);
+        case 'next_keyframe':
+          return f => getNextKeyframe(context, f);
+        case 'prev_keyframe_value':
+          return f => getPreviousKeyframeValue(context, f);
+        case 'next_keyframe_value':
+          return f => getNextKeyframeValue(context, f);
         default:
           throw new Error(`Unrecognised variable ${ast.var_name.value} at ${ast.var_name.start.line}:${ast.var_name.start.col}`);
       }
@@ -100,8 +110,8 @@ export function interpret(ast, context) {
         case 'tri':          
         case 'saw':
         case 'pulse':
-          let [period, phase, amp, centre, pulse] = get_waveform_arguments(ast.arguments).map(arg => interpret(arg, context));
-          return f => wave(ast.fun_name.value, period(f), phase(f)+f, amp(f), centre(f), pulse(f));
+          let [period, phase, amp, centre, pulse] = get_oscillator_named_arguments(ast.arguments).map(arg => interpret(arg, context));
+          return f => oscillator(ast.fun_name.value, period(f), phase(f)+f, amp(f), centre(f), pulse(f));
         case 'min':
           let l1 = interpret(named_argument_extractor(ast.arguments, ['left', 'l'], null), context);
           let r1 = interpret(named_argument_extractor(ast.arguments, ['right', 'r'], null), context);          
@@ -110,9 +120,12 @@ export function interpret(ast, context) {
           let l2 = interpret(named_argument_extractor(ast.arguments, ['left', 'l'], null), context);          
           let r2 =interpret( named_argument_extractor(ast.arguments, ['right', 'r'], null), context);           
           return f => Math.max(l2(f), r2(f))
+          case 'bez':
+            let [x1, y1, x2, y2] = get_bezier_named_arguments(ast.arguments).map(arg => interpret(arg, context));
+            return f => bezier(f, x1(f), y1(f), x2(f), y2(f), context);          
         case 'abs':  
           let v = interpret(named_argument_extractor(ast.arguments, ['value', 'v'], null), context);          
-          return f => Math.abs(v(f))          
+          return f => Math.abs(v(f))
         default:
           throw new Error(`Unrecognised function ${ast.fun_name.value} at ${ast.right.start.line}:${ast.right.start.col}`);
       }
@@ -124,31 +137,37 @@ export function interpret(ast, context) {
           case 'saw':
           case 'pulse':  
             let [period, phase = _ => 0, amp = _ => 1, centre = _ => 0, pulse = _ => 5] = ast.arguments.map(arg => interpret(arg, context));
-            return f => wave(ast.fun_name.value, period(f), phase(f)+f, amp(f), centre(f), pulse(f))
+            return f => oscillator(ast.fun_name.value, period(f), phase(f)+f, amp(f), centre(f), pulse(f))
           case 'min':
             return f => Math.min(interpret(ast.arguments[0], context)(f), interpret(ast.arguments[1], context)(f));
           case 'max':
             return f => Math.max(interpret(ast.arguments[0], context)(f), interpret(ast.arguments[1], context)(f));
           case 'abs':
             return f => Math.abs(interpret(ast.arguments[0], context)(f));
+          case 'bez':
+            let [x1 = _ => 0.5, y1 = _ => 0, x2 = _ => 0.5, y2 = _ => 1] = ast.arguments.map(arg => interpret(arg, context))
+            return f => bezier(f, x1(f), y1(f), x2(f), y2(f), context);
           default:
-            throw new Error(`Unrecognised function ${ast.fun_name.value} at ${ast.right.start.line}:${ast.right.start.col}`);
+            throw new Error(`Unrecognised function ${ast.fun_name.value} at ${ast.start.line}:${ast.start.col}`);
         }
     case "binary_operation":
       let left = interpret(ast.left, context);
       let right = interpret(ast.right, context);
       switch (ast.operator.value) {
-        case '+': return f => left(f)+right(f)
-        case '-': return f => left(f)-right(f)
-        case '*': return f => left(f)*right(f)
-        case '/': return f => left(f)/right(f)
-        case '%': return f => left(f)%right(f)
-        case '==': return f => left(f)==right(f) ? 1 : 0
-        case '<': return f => left(f)<right(f) ? 1 : 0
-        case '<=': return f => left(f)<=right(f) ? 1 : 0
-        case '>=': return f => left(f)>=right(f) ? 1 : 0
-        case '>': return f => left(f)>right(f) ? 1 : 0
-        default: throw new Error(`Unrecognised operator ${ast.operator.value} at ${ast.right.start.line}:${ast.right.start.col}`);
+        case '+': return f => left(f)+right(f);
+        case '-': return f => left(f)-right(f);
+        case '*': return f => left(f)*right(f);
+        case '/': return f => left(f)/right(f);
+        case '%': return f => left(f)%right(f);
+        case '!=': return f => left(f)!=right(f) ? 1 : 0;
+        case '==': return f => left(f)==right(f) ? 1 : 0;
+        case '<': return f => left(f)<right(f) ? 1 : 0;
+        case '<=': return f => left(f)<=right(f) ? 1 : 0;
+        case '>=': return f => left(f)>=right(f) ? 1 : 0;
+        case '>': return f => left(f)>right(f) ? 1 : 0;
+        case 'and': return f => left(f) && right(f) ? 1 : 0;
+        case 'or': return f => left(f) || right(f) ? 1 : 0;
+        default: throw new Error(`Unrecognised operator ${ast.operator.value} at ${ast.operator.start.line}:${ast.operator.start.col}`);
       }
     case "if_expression":
       console.log(ast)
@@ -161,17 +180,45 @@ export function interpret(ast, context) {
   }
 }
 
-function wave(wave, period, pos, amp, centre, pulsewidth) {
-  switch(wave) {
+function getNextKeyframeValue(context, f) {
+  let idx = context.definedFrames.findIndex(v => v > f);
+  return idx ? context.definedValues[idx] : context.definedValues.at(-1);
+}
+
+function getPreviousKeyframeValue(context, f) {
+  let idx = context.definedFrames.findLastIndex(v => v <= f);
+  return idx ? context.definedValues[idx] : context.definedValues.at(0);
+}
+
+function getNextKeyframe(context, f) {
+  return context.definedFrames.find(v => v > f) || context.definedFrames.at(-1);
+}
+
+function getPreviousKeyframe(context, f) {
+  return context.definedFrames.findLast(v => v <= f) || context.definedFrames.at(0);
+}
+
+function oscillator(osc, period, pos, amp, centre, pulsewidth) {
+  switch(osc) {
     case 'sin': return centre + Math.sin(pos * Math.PI * 2 / period) * amp;
     case 'tri': return centre + Math.asin(Math.sin(pos * Math.PI * 2 / period)) * (2 * amp) / Math.PI;
     case 'saw': return centre + (pos % period) * amp / period
     case 'sq':  return centre + (Math.sin(pos * Math.PI * 2 / period) >= 0 ? 1 : -1) * amp;    
     case 'pulse':  return centre + amp *((pos%period) < pulsewidth ? 1 : 0);
-    default:  throw new Error(`Unrecognised waveform ${wave}`);
+    default:  throw new Error(`Unrecognised oscillator ${osc}`);
   }
   
 } 
+
+function bezier(f, x1, y1, x2, y2, context) {
+  let bezier = BezierEasing(x1, y1, x2, y2);
+  let start_x = context.thisKf;
+  let range_x = getNextKeyframe(context, context.thisKf) - getPreviousKeyframe(context, context.thisKf);
+  let start_y = getPreviousKeyframeValue(context, context.thisKf);
+  let range_y = getNextKeyframeValue(context, context.thisKf) - getPreviousKeyframeValue(context, context.thisKf);
+  console.log(range_x, start_y, range_y);
+  return start_y + bezier((f-start_x) / range_x) * range_y;
+}
 
 function named_argument_extractor(args, argNames, defaultValue) {
     let matchingArgs = args.filter( arg => argNames.includes(arg.name.value));
@@ -188,13 +235,22 @@ function named_argument_extractor(args, argNames, defaultValue) {
     }  
   }
 
-function get_waveform_arguments(args) {
+function get_oscillator_named_arguments(args) {
   return [
     named_argument_extractor(args, ['period', 'p'], null),
     named_argument_extractor(args, ['phase', 'ps'],  0),
     named_argument_extractor(args, ['amp', 'a'],  1),
     named_argument_extractor(args, ['centre', 'c'], 0),
     named_argument_extractor(args, ['pulsewidth', 'pulse', 'pw'], 5)
+  ]
+}
+
+function get_bezier_named_arguments(args) {
+  return [
+    named_argument_extractor(args, ['x1'], 0.5),
+    named_argument_extractor(args, ['y1'],  0),
+    named_argument_extractor(args, ['x2'],  0.5),
+    named_argument_extractor(args, ['y2'], 1),
   ]
 }
 
